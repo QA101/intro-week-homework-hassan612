d4db11e8a9948f443046727d7001b374168bac51
Hassan Ismail <hassan612@gmil.com>
2017-10-08 23:41:13 -0700

    Introduction.java

---
+++ week1_solutions/Introduction.java
@@ -0,0 +1,22 @@
+
+public class Introduction {
+
+	public static void main(String[] args) {
+     
+		System.out.println("HI");
+		System.out.println("My name is Hassan");
+		System.out.println("I was born in Syria");
+		System.out.println("I moved to Seattle a while back");
+		System.out.println("and I loved the people here");
+		System.out.println("They are so nice and friendly");
+		System.out.println("So i decided to stay in cloudy skies");
+		System.out.println("and leave sunny southern California");
+		System.out.println("Where it is not as friendly enviroment");
+		System.out.println("as it is here.");
+			
+	}
+	
+	
+	
+	
+}
